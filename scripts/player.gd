4008b53d209ca98cbc5080714a937bce7b925671
Joseph <sourpina@gmail.com>
2025-10-24 23:31:16 -0400

    Created player script with basic movement

---
+++ scripts/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+const SPEED = 125.0
+
+func _physics_process(delta: float) -> void:
+	var horizontal = Input.get_axis("ui_left", "ui_right")
+	var vertical = Input.get_axis("ui_up", "ui_down")
+	var direction = Vector2(horizontal, vertical).normalized() * SPEED * delta
+
+	move_and_collide(direction)
