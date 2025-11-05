92592bacdb45ba83c16452342746bda6144c62da
Joseph Collado <sourpina@gmail.com>
2025-11-04 19:37:23 -0500

    Created basic dialogue display script

---
+++ scripts/dialogue_box.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@export var speaker_title_node: NodePath
+@export var dialogue_text_node: NodePath
+
+@onready var speaker = get_node(speaker_title_node)
+@onready var text = get_node(dialogue_text_node)
+
+const FAST_TEXT_SPEED = 30
+const NORMAL_TEXT_SPEED = 45
+const SLOW_TEXT_SPEED = 60
+
+var text_speed: int = NORMAL_TEXT_SPEED
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	displayText("Joseph", "Testing, testing, 1..2..3..")
+
+func displayText(speaker_name: String, dialogue: String) -> void:
+	speaker.set_text(speaker_name)
+	text.clear()
+	
+	for c in dialogue:
+		text.add_text(c)
+		for i in range(text_speed):
+			await Engine.get_main_loop().process_frame
