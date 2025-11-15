20abe8718ddd71c9d2cf645b847048cad2c63330
Joseph Collado <sourpina@gmail.com>
2025-11-15 18:10:13 -0500

    Added board data variables

--- scripts/slide_board.gd
+++ scripts/slide_board.gd
@@ -1,5 +1,17 @@
 extends Control
 
+@export var board_size: Vector2i
+@export var tile_size: int
+@onready var board: Array[int]
+
+const INDICATOR_1x1 = 3
+const INDICATOR_1x2 = 4
+const FILLER_1x2 = 5
+const INDICATOR_2x1 = 6
+const FILLER_2x1 = 7
+const INDICATOR_2x2 = 8
+const FILLER_2x2 = 9
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
