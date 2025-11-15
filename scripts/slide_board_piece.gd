7e7ec05e8f6cc0aeda9aac43e6f91ebcd5cbe450
Joseph Collado <sourpina@gmail.com>
2025-11-15 18:08:42 -0500

    Changed identifier to piece type enum

--- scripts/slide_board_piece.gd
+++ scripts/slide_board_piece.gd
@@ -1,8 +1,15 @@
 class_name SlideBoardPiece
 extends TextureRect
 
-@export var identifier: int
+@export var piece_type: PieceType
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+enum PieceType {
+	OneByOne,
+	OneByTwo,
+	TwoByOne,
+	TwoByTwo,
+}
