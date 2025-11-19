795ba9432b19cc1d1eb608bbcb1bbe8cf05083e2
Joseph Collado <sourpina@gmail.com>
2025-11-19 18:27:41 -0500

    Removed unused ready function

--- scripts/slide_board_piece.gd
+++ scripts/slide_board_piece.gd
@@ -3,10 +3,6 @@ extends TextureRect
 
 @export var piece_type: PieceType
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 enum PieceType {
 	OneByOne,
 	OneByTwo,
