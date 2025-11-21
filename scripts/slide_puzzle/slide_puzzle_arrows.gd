5b69c9e81bf8ef35dbd78a20b680bf1c51a19d46
Joseph Collado <sourpina@gmail.com>
2025-11-20 23:35:24 -0500

    Fixed hiding on click

---
+++ scripts/slide_puzzle/slide_puzzle_arrows.gd
@@ -0,0 +1,42 @@
+class_name DirectionalArrows
+extends Control
+
+var shown
+
+func _ready() -> void:
+	SlidePuzzleEvents.requested_directions.connect(on_requested_directions)
+	SlidePuzzleEvents.selected_direction.connect(on_selected_direction)
+	SlidePuzzleEvents.hid_directions.connect(hide_arrows)
+	hide_arrows()
+
+func on_requested_directions(pos: Vector2, directions: Array[Globals.Direction]):
+	position = pos + Vector2(4, 4)
+	show_arrows(directions)
+
+func on_selected_direction(direction: Globals.Direction):
+	hide_arrows()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+			_on_mouse_down()
+
+func show_arrows(directions: Array[Globals.Direction]) -> void:
+	shown = true
+	for direction in directions:
+		match direction:
+			Globals.Direction.Left:
+				$LeftArrow.show()
+			Globals.Direction.Right:
+				$RightArrow.show()
+			Globals.Direction.Up:
+				$UpArrow.show()
+			Globals.Direction.Down:
+				$DownArrow.show()
+
+func hide_arrows() -> void:
+	shown = false
+	$LeftArrow.hide()
+	$RightArrow.hide()
+	$UpArrow.hide()
+	$DownArrow.hide()
