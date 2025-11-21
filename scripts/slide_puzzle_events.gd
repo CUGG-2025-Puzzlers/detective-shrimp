46233f545bb5ccad4f9d084325fad7a9bc9e5e9f
Joseph Collado <sourpina@gmail.com>
2025-11-20 23:31:16 -0500

    Moved scripts into slide puzzle scripts folder

--- scripts/slide_puzzle_events.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-signal selected_direction(pos: Vector2, direction: Globals.Direction)
-
-func select_direction(pos: Vector2, direction: Globals.Direction) -> void:
-	selected_direction.emit(pos, direction)
