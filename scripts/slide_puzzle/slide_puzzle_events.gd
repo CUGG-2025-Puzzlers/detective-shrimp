5625737296f94cf3e1399b8f8dbeadbb81322759
Joseph <sourpina@gmail.com>
2025-11-27 03:08:33 -0500

    Added events for puzzle start and completion, and board click

--- scripts/slide_puzzle/slide_puzzle_events.gd
+++ scripts/slide_puzzle/slide_puzzle_events.gd
@@ -1,10 +1,22 @@
 extends Node
 
+signal puzzle_started()
+signal puzzle_completed()
+signal board_clicked()
 signal directions_requested(pos: Vector2, directions: Array[Globals.Direction])
 signal directions_selected(direction: Globals.Direction)
 signal directions_hidden()
 signal piece_moved()
 signal piece_clicked()
+
+func start_puzzle() -> void:
+	puzzle_started.emit()
+
+func complete_puzzle() -> void:
+	puzzle_completed.emit()
+
+func click_board() -> void:
+	board_clicked.emit()
 
 func request_directions(pos: Vector2, directions: Array[Globals.Direction]) -> void:
 	directions_requested.emit(pos, directions)
