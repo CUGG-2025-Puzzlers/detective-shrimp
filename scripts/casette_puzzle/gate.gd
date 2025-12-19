bc42a95e31f3bbda5e2d485887a941ddf96719b7
Joseph Collado <sourpina@gmail.com>
2025-12-19 14:58:12 -0500

    Added basic gate functions with abstract evaluate

--- scripts/casette_puzzle/gate.gd
+++ scripts/casette_puzzle/gate.gd
@@ -1,3 +1,20 @@
 @abstract class_name Gate
 extends TextureRect
 
+@export var inputs: Array[GateInput]
+@export var output: Wire
+
+@abstract func evaluate() -> void
+
+func _ready() -> void:
+	# Listens for input changes from all inputs for this gate
+	for input in inputs:
+		input.stateChanged.connect(_on_input_state_changed)
+
+#region Event Handlers
+
+# Reevaluates the state of this gate whenever one of the inputs changes state
+func _on_input_state_changed() -> void:
+	evaluate()
+
+#endregion
