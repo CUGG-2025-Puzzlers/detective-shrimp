c41dbac40b5e07a3bfcb83b6d28fd2eae257309e
Joseph Collado <sourpina@gmail.com>
2025-12-19 14:53:29 -0500

    Added setup and variables

--- scripts/casette_puzzle/wire.gd
+++ scripts/casette_puzzle/wire.gd
@@ -1,3 +1,17 @@
 class_name Wire
 extends TextureRect
 
+@export var max_length: int
+
+@onready var wire : WireLine
+
+signal stateChanged;
+
+var state = false
+var dragging = false
+var hovered_input: GateInput = null
+
+func _ready() -> void:
+	# Set WireLine reference to child
+	wire = $Wire_Line
+
