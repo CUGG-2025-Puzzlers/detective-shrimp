a315b069d0e11619500b8264e69d26967f4e8eef
Joseph Collado <sourpina@gmail.com>
2025-12-19 14:51:39 -0500

    Added wire line setup

--- scripts/casette_puzzle/wire_line.gd
+++ scripts/casette_puzzle/wire_line.gd
@@ -1,3 +1,14 @@
 class_name WireLine
 extends Line2D
 
+@onready var center
+@onready var max_length
+
+func _ready() -> void:
+	# Adjusts the position to be at the center right of the parent for accurate lines
+	var parent = get_parent()
+	center = parent.size / 2
+	position.x = parent.size.x
+	position.y = center.y
+	max_length = parent.max_length
+
