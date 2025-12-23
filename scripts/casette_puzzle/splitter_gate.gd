ca2cc000d4b2d1ffa07d399194e628f598162c81
Joseph <sourpina@gmail.com>
2025-12-22 21:35:45 -0500

    Fixed outputs not all setting to null when disconnected

--- scripts/casette_puzzle/splitter_gate.gd
+++ scripts/casette_puzzle/splitter_gate.gd
@@ -7,16 +7,18 @@ extends Gate
 func evaluate() -> void:
 	# Nullifies output if more than 1 input
 	if inputs.size() > 1:
-		output.change_state(null)
+		set_output(null)
 		return
 	
 	# Nullfies output if input is null
 	if has_null_connection():
-		output.change_state(null)
+		set_output(null)
 		return
 	
 	# Set outputs to same as input
-	var state = inputs[0].wire.state
+	set_output(inputs[0].wire.state)
+
+func set_output(state):
 	output.change_state(state)
 	for wire in outputs:
 		wire.change_state(state)
