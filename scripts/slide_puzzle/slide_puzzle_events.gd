0f4e7abf87433181430017a131816532a5cf5b31
Joseph <sourpina@gmail.com>
2025-12-26 17:17:33 -0500

    Removed now unused directional arrows

--- scripts/slide_puzzle/slide_puzzle_events.gd
+++ scripts/slide_puzzle/slide_puzzle_events.gd
@@ -2,12 +2,7 @@ extends Node
 
 signal puzzle_started()
 signal puzzle_completed()
-signal board_clicked()
-signal directions_requested(pos: Vector2, directions: Array[Globals.Direction])
-signal directions_selected(direction: Globals.Direction)
-signal directions_hidden()
 signal piece_moved()
-signal piece_clicked()
 
 func start_puzzle() -> void:
 	puzzle_started.emit()
@@ -15,20 +10,5 @@ func start_puzzle() -> void:
 func complete_puzzle() -> void:
 	puzzle_completed.emit()
 
-func click_board() -> void:
-	board_clicked.emit()
-
-func request_directions(pos: Vector2, directions: Array[Globals.Direction]) -> void:
-	directions_requested.emit(pos, directions)
-
-func select_direction(direction: Globals.Direction) -> void:
-	directions_selected.emit(direction)
-
-func hide_directions() -> void:
-	directions_hidden.emit()
-
 func move_piece() -> void:
 	piece_moved.emit()
-
-func click_piece() -> void:
-	piece_clicked.emit()
