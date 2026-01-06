686241008ac55e5ff26da82717aeb8d3044be70a
Joseph <sourpina@gmail.com>
2026-01-06 17:26:56 -0500

    Added print statements

--- scripts/slide_puzzle/slide_puzzle_events.gd
+++ scripts/slide_puzzle/slide_puzzle_events.gd
@@ -4,7 +4,9 @@ signal puzzle_started()
 signal puzzle_completed()
 
 func start_puzzle() -> void:
+	print("Started Slide Puzzle")
 	puzzle_started.emit()
 
 func complete_puzzle() -> void:
+	print("Completed Slide Puzzle")
 	puzzle_completed.emit()
