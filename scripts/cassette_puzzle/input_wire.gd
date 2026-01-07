9709cf134c0948aa2e2969067a7e1c19cfade2f6
Joseph <sourpina@gmail.com>
2026-01-07 15:48:38 -0500

    Changed Wire to be accessed by unique name

--- scripts/cassette_puzzle/input_wire.gd
+++ scripts/cassette_puzzle/input_wire.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 @export var state: bool
 
-@onready var wire: Wire
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	wire = $Wire
-	wire.change_state(state)
+	$%Wire.change_state(state)
