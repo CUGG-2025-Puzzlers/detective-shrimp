4c93fdd055338c8e197230225c0e482b4a94aa11
Joseph <sourpina@gmail.com>
2026-01-09 19:01:04 -0500

    Added general setting for mouse scale

--- scripts/resources/game_settings.gd
+++ scripts/resources/game_settings.gd
@@ -2,6 +2,9 @@
 class_name GameSettings
 extends Resource
 
+# General
+@export var mouse_scale: float = 2.0
+
 # Used in Cassette Puzzle
 @export var on_color: Color = Color(0, 0.2, 1)
 @export var off_color: Color = Color(1, 0, 0.2)
