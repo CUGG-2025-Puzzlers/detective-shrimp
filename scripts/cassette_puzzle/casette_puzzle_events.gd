8a28d265ec7fb08b2e08c020ea7faf4621825d23
Joseph <sourpina@gmail.com>
2026-01-10 19:38:01 -0500

    Added start and complete signals

--- scripts/cassette_puzzle/casette_puzzle_events.gd
+++ scripts/cassette_puzzle/casette_puzzle_events.gd
@@ -1,7 +1,17 @@
 extends Node
 
+signal puzzle_started()
+signal puzzle_completed()
 signal hovered_input(input: GateInput)
 signal unhovered_input()
+
+func start_puzzle() -> void:
+	puzzle_started.emit()
+	print("Started Cassette Puzzle")
+
+func complete_puzzle() -> void:
+	puzzle_completed.emit()
+	print("Completed Cassette Puzzle")
 
 func hover_input(input: GateInput) -> void:
 	hovered_input.emit(input)
