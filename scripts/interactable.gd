270424e7d2b3d9fc244173e8c49d4df35c240591
Joseph <sourpina@gmail.com>
2026-01-15 23:17:47 -0500

    Added enabling/disabling on dialogue close/open respectively

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -9,6 +9,8 @@ var enabled = true
 
 func _ready() -> void:
 	mouse_default_cursor_shape = Control.CURSOR_FORBIDDEN
+	GameEvents.dialogue_started.connect(_on_dialogue_started)
+	GameEvents.dialogue_ended.connect(_on_dialogue_ended)
 
 func _process(delta: float) -> void:
 	if not interactable and is_player_in_range():
@@ -21,6 +23,8 @@ func _process(delta: float) -> void:
 		mouse_default_cursor_shape = Control.CURSOR_FORBIDDEN
 		return
 
+#region Event Handlers
+
 func _gui_input(event: InputEvent) -> void:
 	# Checking for Mouse Button Left events
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -29,6 +33,14 @@ func _gui_input(event: InputEvent) -> void:
 
 func _on_mouse_down() -> void:
 	interact()
+
+func _on_dialogue_started(dialogue: Dialogue) -> void:
+	disable()
+
+func _on_dialogue_ended() -> void:
+	enable()
+
+#endregion
 
 func interact() -> void:
 	if not enabled or not interactable:
