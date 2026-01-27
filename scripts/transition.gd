d1a808e7474b4dae6dc055d372e8830d55f18591
doloknight <59142529+doloknight@users.noreply.github.com>
2026-01-27 18:06:47 -0500

    added transition scene

---
+++ scripts/transition.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+signal on_transition_finished
+
+@onready var color_rect = $ColorRect
+@onready var animation_player = $AnimationPlayer
+
+func ready():
+	color_rect.visible = false
+	
+func _on_animation_finished(anim_name):
+	if anim_name == "fade_to_black":
+		on_transition_finished.emit()
+		animation_player.play("fade_to_scene")
+	if anim_name == "fade_to_scene":
+		color_rect.visible = false
+		animation_player.animation_finished.disconnect()
+
+func transition():
+	animation_player.animation_finished.connect(_on_animation_finished)
+	color_rect.visible = true
+	animation_player.play("fade_to_black")
+	
+func fade_out():
+	color_rect.visible = true
+	animation_player.play("fade_to_black")
