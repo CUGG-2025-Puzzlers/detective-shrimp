894fb6da554e528a74b1d03c31f0141daefcc0c2
Joseph Collado <sourpina@gmail.com>
2026-01-30 16:50:12 -0500

    Added puzzle music swaps on start and finish

--- scripts/puzzle_popup.gd
+++ scripts/puzzle_popup.gd
@@ -3,6 +3,8 @@ extends Control
 @export var puzzle: Puzzle
 @export var puzzle_trigger: GameEvents.PuzzleTrigger
 
+var previous_music: AudioStream
+
 func _ready() -> void:
 	close()
 	GameEvents.puzzle_started.connect(_on_puzzle_started)
@@ -14,11 +16,14 @@ func _on_puzzle_started(trigger: GameEvents.PuzzleTrigger):
 	
 	open()
 	puzzle.start()
+	previous_music = AudioManager.get_current_music()
+	AudioManager.play_background_music(AudioManager.puzzle_theme)
 
 func _on_puzzle_finished(trigger: GameEvents.PuzzleTrigger):
 	if trigger != puzzle_trigger:
 		return
 	
+	AudioManager.play_background_music(previous_music)
 	puzzle.finish()
 	close()
 
