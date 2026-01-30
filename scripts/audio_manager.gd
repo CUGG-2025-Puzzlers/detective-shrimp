37b45ad7a24b4e01a8561d0e8b146964506ec522
Joseph Collado <sourpina@gmail.com>
2026-01-30 16:46:09 -0500

    Changed child reference to be stored in onready variable

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -2,19 +2,21 @@ extends Node
 
 @export var default_fade_time: float = 2.5
 
+@onready var music_player: AudioStreamPlayer = $%MusicPlayer
+
 func _ready() -> void:
-	$%MusicPlayer.finished.connect(_on_music_finished)
+	music_player.finished.connect(_on_music_finished)
 
 func play_background_music(music: AudioStream, fade_time: float = 2.5) -> void:
-	$%MusicPlayer.stop()
-	$%MusicPlayer.stream = music
+	music_player.stop()
+	music_player.stream = music
 	_fade_in_music(fade_time)
 
 func _fade_in_music(fade_time):
-	$%MusicPlayer.volume_linear = 0
-	$%MusicPlayer.play()
+	music_player.volume_linear = 0
+	music_player.play()
 	var tween = create_tween()
-	tween.tween_property($%MusicPlayer, "volume_linear", _get_volume(), fade_time).set_ease(Tween.EASE_IN)
+	tween.tween_property(music_player, "volume_linear", _get_volume(), fade_time).set_ease(Tween.EASE_IN)
 
 func _on_music_finished():
 	await get_tree().create_timer(1).timeout
