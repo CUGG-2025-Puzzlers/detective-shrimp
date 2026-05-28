85417247f193f322fcc14f5f10d59cfbfba83f3a
Joseph <sourpina@gmail.com>
2026-05-28 02:23:37 -0400

    Added comments for function descriptions

--- scripts/light_puzzle/light_bending_puzzle.gd
+++ scripts/light_puzzle/light_bending_puzzle.gd
@@ -24,37 +24,45 @@ func finish() -> void:
 	
 	GameEvents.attempted_reflector_placement.disconnect(_on_attempted_reflector_placement)
 
+# Event listener for when the start button is pressed
+# Fires the light beam
 func _on_start_pressed() -> void:
 	light_source.fire()
 
+# Event listener for when attempting to place a reflector on a tile
+# Returns the reflector back to its pick up position if placed in an invalid tile
+# Leaves it where it was dropped otherwise
 func _on_attempted_reflector_placement(reflector: Reflector, pos: Vector2) -> void:
 	var cell_coords: Vector2 = background_layer.local_to_map(pos)
 	var background_tile_data: TileData = background_layer.get_cell_tile_data(cell_coords)
 	var object_tile_data: TileData = object_layer.get_cell_tile_data(cell_coords)
 	
-	# Put the reflector back to where it was if trying to place out of bounds,
-	# on another puzzle object, on another reflector, or on a wall
+	# Out of bounds
 	if not background_tile_data:
 		print("Cannot place reflector out of bounds")
 		reflector.put_back()
 		return
 	
+	# On another puzzle object
 	if object_tile_data:
 		print("Cannot place reflector on other puzzle object")
 		reflector.put_back()
 		return
 	
+	# On another reflector
 	if is_tile_occupied(reflector):
 		print("Cannot place reflector on another reflector")
 		reflector.put_back()
 		return
 	
+	# On a wall (or other non-placeable tile in the background layer)
 	var can_place: bool = background_tile_data.get_custom_data_by_layer_id(PLACEABILITY_LAYER)
 	if not can_place:
 		print("Cannot place reflector on wall")
 		reflector.put_back()
 		return
 
+# Sets up the reference list of reflectors in the puzzle
 func _setup_reflectors() -> void:
 	_reflectors.clear()
 	for r in reflectors.get_children():
@@ -63,6 +71,8 @@ func _setup_reflectors() -> void:
 		
 		_reflectors.append(r)
 
+# Returns true if a reflector already occupies the given reflector's position
+# Returns false otherwise
 func is_tile_occupied(reflector: Reflector) -> bool:
 	for r in _reflectors:
 		if r == reflector:
