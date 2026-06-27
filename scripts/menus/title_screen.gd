1ba4929e5dfa771257790d8cca56be409b4ac077
Joseph <sourpina@gmail.com>
2026-06-27 15:46:01 -0400

    Updated menus

---
+++ scripts/menus/title_screen.gd
@@ -0,0 +1,28 @@
+extends Menu
+
+@onready var _start_button  : Button = %StartButton
+@onready var _options_button: Button = %OptionsButton
+@onready var _quit_button   : Button = %QuitButton
+
+func _ready() -> void:
+	_start_button.pressed.connect(_on_start_pressed)
+	_options_button.pressed.connect(_on_options_pressed)
+	_quit_button.pressed.connect(_on_quit_pressed)
+
+## Event Handler for when the start button is pressed.
+## Starts the game
+func _on_start_pressed() -> void:
+	print("Starting Shrimptective!")
+	Globals.start_game()
+
+## Event Handler for when the options button is pressed.
+## Opens the Options Menu
+func _on_options_pressed() -> void:
+	print("Options have not yet been implemented... sorry!")
+	pass # Replace with function body.
+
+## Event Handler for when the quit button is pressed.
+## Quits the application
+func _on_quit_pressed() -> void:
+	print("Quitting the game... bye!")
+	get_tree().quit()
