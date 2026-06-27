fe1a5ce2abe10291f5b06b2f06ef84991e56fc5b
Joseph <sourpina@gmail.com>
2026-06-27 02:26:11 -0400

    Removed cutscene root and added hud root

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,11 @@
 extends Node
 
 # World Roots
-@onready var level_root    : Node2D = %LevelRoot
-@onready var cut_scene_root: Node2D = %CutSceneRoot
+@onready var level_root   : Node2D = %LevelRoot
+@onready var entity_root  : Node2D = %EntityRoot
 
 # UI Roots
-@onready var puzzle_root: Control = %PuzzleRoot
+@onready var puzzle_root    : Control = %PuzzleRoot
+@onready var hud_root       : Control = %HUDRoot
 @onready var transition_root: Control = %TransitionRoot
-@onready var pause_root: Control = %PauseRoot
+@onready var pause_root     : Control = %PauseRoot
