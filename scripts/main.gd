4147042ce008a21070acc8a2d8940533728ab25e
Joseph <sourpina@gmail.com>
2026-06-27 02:40:41 -0400

    Stubbed out loading functions

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,3 +9,46 @@ extends Node
 @onready var hud_root       : Control = %HUDRoot
 @onready var transition_root: Control = %TransitionRoot
 @onready var pause_root     : Control = %PauseRoot
+
+# Scene UIDs
+const MAIN_MENU_UID: String = "uid://dkrxhuquv6u7e"
+const PLAYER_UID   : String = "uid://dknb10beku6mq"
+
+var player: Player = null
+
+func _ready() -> void:
+	load_scene(MAIN_MENU_UID)
+
+## Initializes the player
+func _init_player() -> void:
+	var player_scene: PackedScene = ResourceLoader.load(PLAYER_UID) as PackedScene
+	if player_scene == null:
+		var error: String = "Could not load player scene: %s" % PLAYER_UID
+		push_error(error)
+		print(error)
+		return
+	
+	player = player_scene.instantiate() as Player
+	if player == null:
+		var error: String = "Loaded player scene does not extend player or does not exist: %s" % PLAYER_UID
+		push_error(error)
+		print(error)
+		return
+	
+	entity_root.add_child(player)
+
+## Loads a menu on top of the current scene (if there is one)
+func load_menu(menu_uid: String) -> void:
+	pass
+
+## Loads a new game scene in the world, specifically cutscenes and rooms
+func load_scene(scene_uid: String) -> void:
+	_deferred_load_scene.call_deferred(scene_uid)
+
+## Does the actual scene loading during idle time
+func _deferred_load_scene(scene_uid: String) -> void:
+	pass
+
+## Stops the current scene (if there is one) and loads a puzzle
+func load_puzzle() -> void:
+	pass
