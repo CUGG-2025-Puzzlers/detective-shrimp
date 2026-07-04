1a28c8ea3e3be91c13b1ba117348569cc46164d9
Joseph <sourpina@gmail.com>
2026-07-03 22:03:15 -0400

    Updated to use global GameEvents instead of local signals

--- scripts/puzzle_instructions.gd
+++ scripts/puzzle_instructions.gd
@@ -1,27 +1,33 @@
+class_name PuzzleIntructions
 extends NinePatchRect
 
 @export var info: PuzzleInfo
 
-@onready var title: RichTextLabel = $PuzzleTitle
-@onready var description: RichTextLabel = $DescriptionText
-@onready var objective: RichTextLabel = $ObjectiveText
-@onready var restart_button: Button = $RestartButton
-@onready var quit_button: Button = $QuitButton
+@onready var _title      : RichTextLabel = $PuzzleTitle
+@onready var _description: RichTextLabel = $DescriptionText
+@onready var _objective  : RichTextLabel = $ObjectiveText
 
-signal puzzle_restarted
-signal puzzle_quit
+@onready var _restart_button: Button = $RestartButton
+@onready var _quit_button   : Button = $QuitButton
 
 func _ready() -> void:
+	_restart_button.pressed.connect(_on_restart_pressed)
+	_quit_button.pressed.connect(_on_quit_pressed)
+	
 	if not info:
 		print("[Puzzle Instructions] Warning: No info set")
 		return
 	
-	title.text = "[b]%s[/b]" % info.title
-	description.text = info.description
-	objective.text = info.objective
+	_title.text = "[b]%s[/b]" % info.title
+	_description.text = info.description
+	_objective.text = info.objective
 
+## Event Handler for when the Restart button is pressed.
+## Requests that the puzzle be restarted
 func _on_restart_pressed() -> void:
-	puzzle_restarted.emit()
+	GameEvents.request_puzzle_restart()
 
+## Event Handler for when the Quit button is pressed.
+## Requests that the puzzle be quit
 func _on_quit_pressed() -> void:
-	puzzle_quit.emit()
+	GameEvents.request_puzzle_close()
