c804229dbcdbe79d7eb334ec0458ccdfcb4f5748
Joseph <sourpina@gmail.com>
2026-08-18 11:19:41 -0400

    Added static constructor

--- scripts/cryptogram_puzzle/cryptogram_char_slot.gd
+++ scripts/cryptogram_puzzle/cryptogram_char_slot.gd
@@ -1,9 +1,17 @@
 class_name CryptogramCharSlot
 extends Control
 
+const CHAR_SLOT_SCENE: PackedScene = preload("res://scenes/cryptogram_puzzle/cryptogram_char_slot.tscn")
+
 var is_editable: bool
 var is_selected: bool
 var letter_group: CryptogramGroup
+
+static func new(editable: bool, group: CryptogramGroup) -> CryptogramCharSlot:
+	var char_slot = CHAR_SLOT_SCENE.instantiate()
+	char_slot.is_editable = editable
+	char_slot.letter_group = group
+	return char_slot
 
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
