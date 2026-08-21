6e44a873639b2672abc0cf50ab597c078f856033
Joseph Collado <sourpina@gmail.com>
2026-08-21 16:56:49 -0400

    Added restart button disable on puzzle complete

--- scripts/puzzle_instructions.gd
+++ scripts/puzzle_instructions.gd
@@ -13,6 +13,7 @@ extends NinePatchRect
 func _ready() -> void:
 	_restart_button.pressed.connect(_on_restart_pressed)
 	_quit_button.pressed.connect(_on_quit_pressed)
+	GameEvents.puzzle_complete_requested.connect(_on_puzzle_complete_requested)
 	
 	if not info:
 		print("[Puzzle Instructions] Warning: No info set")
@@ -21,6 +22,7 @@ func _ready() -> void:
 	_title.text = "[b]%s[/b]" % info.title
 	_description.text = info.description
 	_objective.text = info.objective
+	_restart_button.disabled = false
 
 ## Event Handler for when the Restart button is pressed.
 ## Requests that the puzzle be restarted
@@ -31,3 +33,8 @@ func _on_restart_pressed() -> void:
 ## Requests that the puzzle be quit
 func _on_quit_pressed() -> void:
 	GameEvents.request_puzzle_close()
+
+## Event Handler for when the current puzzle has been completed
+## Disables the restart button
+func _on_puzzle_complete_requested() -> void:
+	_restart_button.disabled = true
